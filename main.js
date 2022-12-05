0b16e586363b6916c62dc62520256699acf05de1
Ollie Treend <ollie.treend@digital.cabinet-office.gov.uk>
2022-12-05 13:02:32 +0000

    Add some default content to the editor

--- main.js
+++ main.js
@@ -2,8 +2,19 @@ import './style.css'
 import EditorJS from '@editorjs/editorjs';
 
 const editor = new EditorJS({
-  /**
-   * Id of Element that should contain the Editor
-   */
   holder: 'editorjs',
+  placeholder: "Write something inspirational...",
+  data: {
+    "time": 1670245063307,
+    "blocks": [
+      {
+        "id": "HW5xpJz577",
+        "type": "paragraph",
+        "data": {
+          "text": "Something inspirational"
+        }
+      }
+    ],
+    "version": "2.26.0"
+  },
 });
