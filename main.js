094c3f1c24b5656cc9b6973434ffe6c034243029
Ollie Treend <ollie.treend@digital.cabinet-office.gov.uk>
2022-12-05 13:49:51 +0000

    Render JSON output from the editor

--- main.js
+++ main.js
@@ -3,6 +3,12 @@ import sampleData from './sample-data.json';
 import EditorJS from '@editorjs/editorjs';
 import Header from '@editorjs/header';
 
+const outputTo = document.querySelector('#output code');
+const renderOutput = async function (api) {
+  const data = await api.saver.save();
+  outputTo.innerText = JSON.stringify(data, null, 2);
+}
+
 const editor = new EditorJS({
   holder: 'editorjs',
 
@@ -12,4 +18,6 @@ const editor = new EditorJS({
 
   placeholder: "Write something inspirational...",
   data: sampleData,
+
+  onChange: renderOutput,
 });
