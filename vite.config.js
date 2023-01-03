5dd0b82c97c717d0d36991b3ee1df8927edfdc4b
Ollie Treend <ollie.treend@digital.cabinet-office.gov.uk>
2023-01-03 15:26:02 +0000

    Add Svelte support

--- vite.config.js
+++ vite.config.js
@@ -1,6 +1,10 @@
 import { defineConfig } from 'vite';
+import { svelte } from '@sveltejs/vite-plugin-svelte';
 
 export default defineConfig({
+  plugins: [
+    svelte(),
+  ],
   test: {
     globals: true,
     environment: 'jsdom',
