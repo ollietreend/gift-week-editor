e067a62a608c02a4430014ab48c554dff9fcee9c
Ollie Treend <ollie.treend@digital.cabinet-office.gov.uk>
2023-01-03 15:08:15 +0000

    Create an Image tool which supports cropping
    This commit creates an Image tool which requires images to be exactly
    960x640 pixels. If they aren't, it'll show a cropping tool so the user
    can crop the image to the correct dimensions.

---
+++ editor/tools/image/components/BrowseButton.svelte
@@ -0,0 +1,27 @@
+<script>
+  import ajax from '@codexteam/ajax';
+  import { IconPicture } from '@codexteam/icons';
+
+  export let file;
+
+  const browse = async () => {
+    const files = await ajax.selectFiles({
+      accept: 'image/png, image/jpeg, image/gif, image/svg+xml',
+      multiple: false,
+    });
+    file = files[0];
+  };
+</script>
+
+<div class="cdx-button" on:click={browse}>
+  {@html IconPicture}
+  Select an Image
+</div>
+
+<style>
+  .cdx-button {
+    display: flex;
+    align-items: center;
+    justify-content: center;
+  }
+</style>
